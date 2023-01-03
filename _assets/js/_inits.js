1e11d6fe069c2931b474d4e43472cc4d2117fc32
Gigi <dergigi@pm.me>
2023-01-04 00:19:56 +0100

    Add layouts and includes

---
+++ _assets/js/_inits.js
@@ -0,0 +1,15 @@
+// ----------------------------------------------
+// Imports
+// ----------------------------------------------
+import $ from 'jquery';
+import './vendor/_transition.js';
+import PageTransition from './components/_pageTransition.js';
+
+// ----------------------------------------------
+// Inits
+// ----------------------------------------------
+$(() => {
+
+  PageTransition.init();
+
+});
